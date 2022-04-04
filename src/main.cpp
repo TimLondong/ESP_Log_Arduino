3257443586fd5e343022beabb449c351d3fb8405
Tim Londong <londong@revotion.de>
2022-04-04 17:34:05 +0200

    comment log

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,10 @@ void setup()
 
 void loop()
 {
-  log_v("Verbose");
-  log_d("Debug");
-  log_i("Info");
-  log_w("Warning");
-  log_e("Error");
+  log_v("Verbose"); //use this for description 
+  log_d("Debug");   //use this to print out Variabels
+  log_i("Info");    //use this for Informations
+  log_w("Warning"); //should be clear
+  log_e("Error");   //should be clear
   delay(10000);
 }
